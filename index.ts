a6c9735492508262ee1cd03365e8e61949ae605d
mdepasquale <depasquale@esteco.com>
2022-03-09 20:03:00 +0100

    refactor index.ts

--- index.ts
+++ index.ts
@@ -4,24 +4,26 @@ import {Transaction} from "./blockchain/interfaces/transaction";
 const difficulty = 2;
 const blockchain: Blockchain = Blockchain.create(difficulty);
 
-const transaction1: Transaction = {
-    from: 'Marco',
-    to: 'Nicola',
-    amount: 7
-};
-const transaction2: Transaction = {
-    from: 'Daniele',
-    to: 'Matteo',
-    amount: 100
-};
-const transaction3: Transaction = {
-    from: 'Lorenzo',
-    to: 'Laura',
-    amount: 50
-};
-blockchain.addBlock(transaction1);
-blockchain.addBlock(transaction2);
-blockchain.addBlock(transaction3);
+const transactions: Transaction[] = [
+    {
+        from: 'Marco',
+        to: 'Nicola',
+        amount: 7
+    },
+    {
+        from: 'Daniele',
+        to: 'Matteo',
+        amount: 100
+    },
+    {
+        from: 'Lorenzo',
+        to: 'Laura',
+        amount: 50
+    }
+];
+for (let transaction of transactions) {
+    blockchain.addBlock(transaction);
+}
 
 blockchain.printToConsole();
 
